f5d3a5636b8275928834a87ce71af361b0462f21
Muhammad Rivaldhi <mhmmdrivaldhi10@gmail.com>
2026-01-12 00:13:09 +0700

    [Session 3] integrate category, product, and product detail API's

--- app/(landing)/product/[id]/page.tsx
+++ app/(landing)/product/[id]/page.tsx
@@ -1,33 +1,42 @@
 import Image from "next/image";
 import ProductActions from "../../components/product-detail/product-actions";
 import PriceFormatter from "@/app/utils/price-formatter";
+import { getProductDetail } from "@/app/services/product_service";
+import { getImageUrl } from "@/app/lib/api";
 
-const ProductDetail = () => {
+type TPageProps = {
+    params: Promise<{id: string}>
+}
+
+const ProductDetail = async ({params}: TPageProps) => {
+    const {id} = await params
+    const product = await getProductDetail(id);
+
     return (
         <main className="container mx-auto flex flex-col lg:flex-row lg:gap-12 gap-10 lg:px-16 px-4 py-10 lg:py-14">
             <div className="bg-primary-light aspect-square  lg:min-w-[560px] flex justify-center items-center">
                 <Image
-                    src="/images/products/football-shoes.png"
-                    alt="Football Shoes"
+                    src={getImageUrl(product.imageUrl)}
+                    alt={product.name}
                     width={564}
                     height={378}
                     className="aspect-square object-contain w-full"
                 />        
-            </div>
+            </div> 
             <div className="w-full lg:py-7 lg:px-7">
-                <h1 className="font-bold lg:text-4xl text-2xl mb-5">SportsOn HyperSoccer v2</h1>
+                <h1 className="font-bold lg:text-4xl text-2xl mb-5">{product.name}</h1>
                 <div className="bg-primary-light rounded-full text-primary py-2 px-6 w-fit lg:mb-8 mb-4">
-                    Football
+                    {product.category.name}
                 </div>
                 <p className="leading-loose">
-                    The SportsOn HyperSoccer v2 is engineered for the player who demands precision, power, and unrivaled speed on the pitch. Featuring a striking, two-toned black and white design with deep crimson accents, these cleats don't just perform they make a statement. Experience the future of football footwear with v2's enhanced fit and cutting-edge traction.
+                    {product.description}
                 </p>
                 <div className="text-primary lg:text-[32px] text-2xl font-semibold lg:py-10 py-5">
                     {
-                        PriceFormatter(458000)
+                        PriceFormatter(product.price)
                     }
                 </div>
-                <ProductActions />
+                <ProductActions stock={product.stock}/>
             </div>
         </main>
     )
---
+++ app/lib/api.ts
@@ -0,0 +1,27 @@
+export async function fetchAPI<T>(
+    endpoint: string,
+    option?: RequestInit,
+): Promise<T> {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
+        ...option,
+        cache: option?.cache || "no-store",
+    })
+
+    if (!res.ok) {
+        let errorMessage = `Failed to fetch data from ${endpoint}`
+        try {
+            const errorData = await res.json();
+            errorMessage = errorData.message || errorData.error || errorMessage;
+        } catch (e) {
+            console.log(e)
+        }
+
+        throw new Error(errorMessage)
+    }
+    return res.json()
+}
+
+export function getImageUrl(path: string) {
+    if (path.startsWith("http")) return path;
+    return `${process.env.NEXT_PUBLIC_API_ROOT}/${path}`
+}
